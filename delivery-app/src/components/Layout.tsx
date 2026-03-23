709a3de437af174cb22a013742d4505b186659e9
Franklin Pereira <franklin.dev.full@gmail.com>
2026-03-23 11:00:47 -0300

    fix:jsx tag

--- delivery-app/src/components/Layout.tsx
+++ delivery-app/src/components/Layout.tsx
@@ -164,8 +164,6 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
                 </div>
             ) : (
                 <>
-                        </div>
-                    )}
 
                     {/* Sidebar Overlay */}
                     <div
