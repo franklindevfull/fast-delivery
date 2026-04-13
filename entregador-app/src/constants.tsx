70fc200ef9f85a78465a2bb25e17dcd034fef20e
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2026-04-13 19:30:25 -0300

    fix:PixInfoModal.tsx imports

--- entregador-app/src/constants.tsx
+++ entregador-app/src/constants.tsx
@@ -1,4 +1,6 @@
 
+import { QrCode, Copy } from 'lucide-react';
+
 export const Colors = {
     primary: '#2563eb', // Blue 600
     secondary: '#10b981', // Emerald 500
@@ -103,5 +105,7 @@ export const Icons = {
         <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" {...props}>
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-    )
+    ),
+    QrCode: (props: any) => <QrCode {...props} />,
+    Copy: (props: any) => <Copy {...props} />
 };
